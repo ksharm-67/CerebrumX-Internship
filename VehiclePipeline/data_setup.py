d63d45ccf6ff008cd8a9ec9544568a6edc2a7796
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-08-30 03:08:19 -0700

    update geofence and driver data

--- VehiclePipeline/data_setup.py
+++ VehiclePipeline/data_setup.py
@@ -1,11 +1,14 @@
 from pymongo import MongoClient
 import json
 
+#Connect to your MongoDB Database
 client = MongoClient("mongodb://localhost:27017/")
 
+#Define the database and driver data collection
 db = client["vehicle_telemetry"]
 driv = db["driver_data"]
 
+#From a json file, send driver data to your Mongo database
 try:
     with open("driverData.json", "r") as f:
         data = json.load(f)
@@ -15,8 +18,11 @@ try:
 except Exception as e:
     print(f"Error: {e}")
     
-
+    
+#Define the geofence collection
 geo = db["geofences"]
+
+#From a json file, define the geofence boundaries
 try:
     with open("geofences.json", "r") as g:
         reg = json.load(g)
@@ -26,6 +32,7 @@ try:
 except Exception as ex:
     print(f"Error: {ex}")
     
+#Create a 2dsphere index for efficiency
 db.geofences.create_index([("polygon", "2dsphere")]) 
 
 
